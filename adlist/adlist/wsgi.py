c6416cf8080ed78d3d570244242ffeb0c4615b89
Shaung Cheng <shaungc@umich.edu>
2019-03-22 13:41:12 -0400

    scaffold adlist project

--- adlist/adlist/wsgi.py
+++ adlist/adlist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for unesco project.
+WSGI config for adlist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unesco.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adlist.settings')
 
 application = get_wsgi_application()
